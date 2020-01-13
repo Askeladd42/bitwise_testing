cb2f1e6279a93bebb828865213622d6b607d0496
Pierre-marie LAM <plam@e2r8p10.42.fr>
2020-01-13 17:11:53 +0100

    all exercises done

--- test6.c
+++ test6.c
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/12 12:31:56 by plam              #+#    #+#             */
-/*   Updated: 2020/01/12 13:19:33 by plam             ###   ########.fr       */
+/*   Updated: 2020/01/13 17:11:16 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,14 @@ int	main()
 	int	nth;
 	int newN;
 
-	N = 22;
-	nth = 4;
+	N = 0;
+	nth = 8;
+	int	nth2 = 4;
 	printf("Number checked : %d\n", N);
 	printf("nth bit to toggle (0-31): %d\n", nth);
 	newN = N ^ (1 << nth);
 	printf("Number after toggling %d bit: %d (in decimal)\n", nth, newN);
+	newN ^= (1 << nth2); 
+	printf("Number after toggling %d and %d bit: %d (in decimal)\n", nth, nth2, newN);
 	return (0);
 }
